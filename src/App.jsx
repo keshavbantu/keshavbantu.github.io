0ab9b204c5bf1e654f87110b5cfd0bfaf6738a97
keshav bantu <keshavbantu.2@gmail.com>
2025-04-27 19:09:49 +0530

    links and base

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
+import './App.scss'
+import Links from './links/Links'
 
 function App() {
   return (
-    <> 
-      <h1>hi</h1>
-    </>
+    <div className='container'>
+      <div className='header'>
+        <h1>Keshav Bantwal</h1>
+        <p><span>[alias]</span> bantuboi · notbantuboi · cy3erg33k · keshav bantu</p>
+      </div>
+      <div className='subtext'>
+        <p>{`< Engineer. Builder. Artisan. />`}</p>
+      </div>
+      <Links />
+    </div>
   )
 }
 
